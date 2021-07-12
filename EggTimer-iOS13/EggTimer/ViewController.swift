fdee55b3b6471f6bda7f6fba1a2ed870501f78a0
Brandon Palomino <42811121+bpalomino5920@users.noreply.github.com>
2021-07-12 09:29:53 -0400

    Add files via upload

---
+++ EggTimer-iOS13/EggTimer/ViewController.swift
@@ -0,0 +1,69 @@
+//
+//  ViewController.swift
+//  EggTimer
+//
+//  Created by Angela Yu on 08/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    let eggTimes = ["Soft": 3, "Medium": 4, "Hard": 7] // in seconds, not  minutes (60sec x min = amount of secs)
+    
+    var timer = Timer()
+    
+    var totalTime = 0
+    var secondsPassed = 0
+    
+    var player: AVAudioPlayer!
+    
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+
+        timer.invalidate()
+        
+        let hardness = sender.currentTitle! //Soft, Medium, Hard
+        
+        totalTime = eggTimes[hardness]!
+        
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        titleLabel.text = hardness
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        
+    }
+    
+    @objc func updateTimer() {
+        //example functionality
+        if secondsPassed < totalTime {
+            
+            secondsPassed += 1
+            
+            progressBar.progress = Float(secondsPassed) / Float(totalTime)
+            
+            print(Float(secondsPassed) / Float(totalTime))
+        
+            
+            
+        } else { //print DONE! when it hits zero
+            timer.invalidate()
+            titleLabel.text = "DONE!"
+            
+            //play alarm sound
+            let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+                        player = try! AVAudioPlayer(contentsOf: url!)
+                        player.play()
+            
+        }
+    }
+
+
+}
